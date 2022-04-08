a4c64ef69e93d531d8b7621e1905cc8296d7ef27
Jwvd <40066149+CaptKitty@users.noreply.github.com>
2022-04-09 00:25:50 +0200

    Revert "Merge branch 'master' of https://github.com/Stiopa866/Governments-Expanded"
    This reverts commit 3fa16a22bbf7118d9b4bcdaceb49ba00ba8b777d, reversing
    changes made to c50247e728a97007273eaaa8d87298cef3574b4d.

--- common/defines/government_expanded_defines.lua
+++ common/defines/government_expanded_defines.lua
@@ -1,3 +1,2 @@
 NDefines.NGovernment.GOVERNMENT_REFORM_COST_INCREASE = 25
-NDefines.NCountry.STATE_PRIVILEGE_ADMIN_COST = 1			-- Cost of granting a privilege
-NDefines.NCountry.ESTATE_PRIVILEGES_MAX_CONCURRENT = 8		-- Max. number of privileges active at the same time
+NDefines.NGovernment.GOVERNMENT_REFORM_CHANGE_CORRUPTION = 5.0
